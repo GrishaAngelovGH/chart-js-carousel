94ef22ce72a20271f87fb8879902d8573d6a7284
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-22 10:25:59 +0300

    Enable keyboard navigation for swiper carousel

--- src/swiper.js
+++ src/swiper.js
@@ -1,11 +1,14 @@
 import Swiper from 'swiper'
-import { Navigation } from 'swiper/modules'
+import { Navigation, Keyboard } from 'swiper/modules'
 import 'swiper/modules/navigation.css'
 import 'swiper/swiper.css'
 
 new Swiper('.swiper', {
-  modules: [Navigation],
+  modules: [Navigation, Keyboard],
   loop: true,
+  keyboard: {
+    enabled: true
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
