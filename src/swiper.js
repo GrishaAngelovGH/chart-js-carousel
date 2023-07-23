8df43bc753b128f2c8adb0f55673b238bca6c298
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-23 12:59:49 +0300

    Enable pagination for slides of the carousel

--- src/swiper.js
+++ src/swiper.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper'
-import { Navigation, Keyboard } from 'swiper/modules'
+import { Navigation, Keyboard, Pagination } from 'swiper/modules'
 import 'swiper/modules/navigation.css'
+import 'swiper/modules/pagination.css'
 import 'swiper/swiper.css'
 
 new Swiper('.swiper', {
-  modules: [Navigation, Keyboard],
+  modules: [Navigation, Keyboard, Pagination],
   loop: true,
   keyboard: {
     enabled: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
   },
   navigation: {
     nextEl: '.swiper-button-next',
