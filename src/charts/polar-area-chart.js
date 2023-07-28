0d86f3c7228c178d8e4fdb13312ea964dbcf3b1a
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-28 09:43:59 +0300

    Implement polar area chart

---
+++ src/charts/polar-area-chart.js
@@ -0,0 +1,43 @@
+import createContext from './chart-context'
+import Chart from 'chart.js/auto'
+
+Chart.defaults.font.size = 20
+
+const ctx = createContext('polar-area-chart')
+
+new Chart(ctx, {
+  type: 'polarArea',
+  data: {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow',
+      'Grey',
+      'Blue'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [11, 16, 7, 3, 14],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  },
+  options: {
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+    plugins: {
+      title: {
+        display: true,
+        text: 'Polar Area Chart',
+        font: {
+          size: 40
+        }
+      }
+    }
+  }
+})
